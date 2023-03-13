1fada84f34dec5994309013ee5209b541864d953
martin_ding <1150354658@qq.com>
2023-03-13 11:42:05 +0800

    optimize pom

--- src/test/java/org/brainstorm/service/impl/DataGenerateStrategyServiceImplTest.java
+++ src/test/java/org/brainstorm/service/impl/DataGenerateStrategyServiceImplTest.java
@@ -3,15 +3,10 @@ package org.brainstorm.service.impl;
 import org.brainstorm.interfaces.strategy.Type;
 import org.brainstorm.service.DataGenerateStrategyService;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
 class DataGenerateStrategyServiceImplTest {
 
     @Autowired
--- src/test/java/org/brainstorm/service/impl/RestTemplateHandlerTest.java
+++ src/test/java/org/brainstorm/service/impl/RestTemplateHandlerTest.java
@@ -2,17 +2,13 @@ package org.brainstorm.service.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.minidev.json.JSONObject;
-import org.brainstorm.model.Student;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.*;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
 class RestTemplateHandlerTest {
     @Autowired
     private RestTemplate restTemplate;
